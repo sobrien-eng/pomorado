51afdba9bf9644ce7c61b6bad88d3380e85f191b
sobrien-eng <sobrien@student.neumont.edu>
2022-08-30 10:23:04 -0600

    asdfghjk

--- timer.js
+++ timer.js
@@ -3,23 +3,28 @@
 //var restTimer
 //grab ids from buttons 
 
-var countDown = new Date().getTime();
+
+
+//store a reference to a timer variable
+var startTimer;
+
+var currentTime = new Date().getTime();
 var interval = setInterval(function () {
     var now = new Date().getTime();
     var difference = countDown - now;
 
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-    document.getElementById("").innerHTML = days + "d " + hours + "h "
+    document.getElementById("").innerHTML = hours + "h "
         + minutes + "m " + seconds + "s ";
 
     // If the count down is finished, write some text
-    if (distance < 0) {
+    if (difference < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("demo").innerHTML = "Session complete!";
     }
 }, 1000);
 
