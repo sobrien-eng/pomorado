e64537f93ccd163b9f763ccb210ab9a55c5c1644
sobrien-eng <sobrien@student.neumont.edu>
2022-08-31 01:36:12 -0600

    m

--- timer.js
+++ timer.js
@@ -8,23 +8,35 @@
 //store a reference to a timer variable
 var startTimer;
 
-var currentTime = new Date().getTime();
-var interval = setInterval(function () {
-    var now = new Date().getTime();
-    var difference = countDown - now;
-
-    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-    // Display the result in the element with id="demo"
-    document.getElementById("").innerHTML = hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-    // If the count down is finished, write some text
-    if (difference < 0) {
+var deadline = new Date() ;
+  
+var x = setInterval(function() {
+  
+var now = new Date().getTime();
+var t = deadline - now;
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("hour").innerHTML =hours;
+document.getElementById("minute").innerHTML = minutes; 
+document.getElementById("second").innerHTML =seconds; 
+if (t < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "Session complete!";
-    }
+        document.getElementById("demo").innerHTML = "Session Complete";
+        
+        document.getElementById("hour").innerHTML ='0';
+        document.getElementById("minute").innerHTML ='0' ; 
+        document.getElementById("second").innerHTML = '0'; }
 }, 1000);
+
+//     // Display the result in the element with id="demo"
+//     document.getElementById("").innerHTML = hours + "h "
+//         + minutes + "m " + seconds + "s ";
+
+//     // If the count down is finished, write some text
+//     if (difference < 0) {
+//         clearInterval(x);
+//         document.getElementById("demo").innerHTML = "Session complete!";
+//     }
+// }, 1000);
 
